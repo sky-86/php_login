2a1483fc4c8a79ee0e53125d905b8b2c5ebbad14
sky-86 <sky86one@gmail.com>
2022-07-29 23:04:44 -0500

    >

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,12 @@
 <?php
 
-$server_name = "localhost";
+$server_name = "containers-us-west-35.railway.app";
 $db_username = "root";
-$db_password = "";
-$db_name = "login_system";
+$db_password = "O9IMQ0WCDNn3hs78vHeL";
+$db_name = "railway";
+$port = "7926";
 
-$conn = mysqli_connect($server_name, $db_username, $db_password, $db_name);
+$conn = mysqli_connect($server_name, $db_username, $db_password, $db_name, $port);
 
 if (!$conn) {
   die("Connection Failed: ".mysqli_connect_error());
